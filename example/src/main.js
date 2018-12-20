a4d653551213f2312cb04486db0fb354b4f872c2
smirzo <sivlermirzo@gmail.com>
2018-12-20 05:44:45 +0100

    adds example project

---
+++ example/src/main.js
@@ -0,0 +1,11 @@
+// Imports dependencies.
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
+
+// Sets up Vue.
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  store,
+});
