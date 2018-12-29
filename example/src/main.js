6b920eaad584e1fd8cb63a50c4fb515e16bb2cd2
smirzo <sivlermirzo@gmail.com>
2018-12-29 07:20:17 +0100

    tries reverting to the previous version of the sandbox

--- example/src/main.js
+++ example/src/main.js
@@ -2,7 +2,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-import packageJSON from '../package.json'
 
 // Sets up Vue.
 new Vue({
@@ -10,5 +9,3 @@ new Vue({
   render: h => h(App),
   store
 })
-
-console.log(`Current version: ${packageJSON.dependencies['vuex-observable-plugin']}`)
